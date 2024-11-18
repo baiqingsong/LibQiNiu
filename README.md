c5f9b86e855281e698b76f1aba1b6847c8a6f15c
dawnbai <904494377@qq.com>
2024-11-18 17:55:32 +0800

    测试程序
    添加测试程序，可以正常上传本地图片到七牛云上

--- README.md
+++ README.md
@@ -6,3 +6,24 @@
 * getInstance 获取七牛云上传对象
 * uploadFile 上传文件
 * uploadImage 上传图片
+
+1. AndroidManifest.xml
+```
+    <uses-permission android:name="android.permission.INTERNET"/>
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+```
+```
+    android:networkSecurityConfig="@xml/network_security_config"
+```
+2. res/xml/network_security_config.xml
+```
+    <?xml version="1.0" encoding="utf-8"?>
+    <network-security-config>
+        <base-config cleartextTrafficPermitted="true" />
+    </network-security-config>
+```
+3. build.gradle
+```
+    implementation 'com.google.code.gson:gson:2.8.5'
+    implementation 'com.squareup.okhttp3:okhttp:4.2.2'
+```
--- app/src/main/java/com/dawn/libqiniu/MainActivity.java
+++ app/src/main/java/com/dawn/libqiniu/MainActivity.java
@@ -2,13 +2,60 @@ package com.dawn.libqiniu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.dawn.qiniu.QiNiuFactory;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvUpload;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvUpload = findViewById(R.id.tv_upload);
+        qiniuInit();
+        // 将drawable中的图片转换成bitmap
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.demo_1);
+        tvUpload.setOnClickListener(view -> {
+            qiniuUploadImage(bitmap);
+        });
+    }
+    public static final String ACCESS = "";
+    public static final String SECRET = "";
+    public static final String BUCKET = "";//区域
+    public static final String PHOTO_HOST = "";
+    QiNiuFactory qiNiuFactory;
+    private void qiniuInit(){
+        qiNiuFactory = QiNiuFactory.getInstance(ACCESS, SECRET, BUCKET, PHOTO_HOST);
+    }
+
+    private void qiniuUploadImage(Bitmap bitmap){
+        String key = "img/" + System.currentTimeMillis() + new Random().nextInt(1000) + ".jpg";
+        Log.e("dawn", "key: " + key);
+        qiNiuFactory.uploadImage(bitmap, key, new QiNiuFactory.QiNiuUploadListener() {
+            @Override
+            public void uploadPercent(float percent) {
+                Log.e("dawn", "percent: " + percent);
+            }
+
+            @Override
+            public void uploadSuccess(String url) {
+                Log.e("dawn", "url: " + url);
+            }
+
+            @Override
+            public void uploadFail() {
+                Log.e("dawn", "uploadFail");
+            }
+        });
     }
 }
